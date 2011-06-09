ab528b69c21696445c7b840554b14fb80221d6f7
gclue.akira <gclue.akira@518bc17e-d005-4b43-69f7-0d2ff808def2>
2011-06-09 22:11:41 +0000

    Add reload button

--- trunk/GeigerClient/gen/org/fukushima/OpenGeiger/R.java
+++ trunk/GeigerClient/gen/org/fukushima/OpenGeiger/R.java
@@ -18,17 +18,18 @@ public final class R {
         public static final int pin=0x7f020004;
     }
     public static final class id {
-        public static final int Button02=0x7f050009;
-        public static final int Button03=0x7f05000a;
-        public static final int buttonUp=0x7f050005;
+        public static final int Button02=0x7f05000a;
+        public static final int Button03=0x7f05000b;
+        public static final int buttonReload=0x7f050005;
+        public static final int buttonUp=0x7f050006;
         public static final int device=0x7f050001;
         public static final int editText=0x7f050003;
-        public static final int icon_bluetooth=0x7f050006;
-        public static final int icon_geiger=0x7f050007;
+        public static final int icon_bluetooth=0x7f050007;
+        public static final int icon_geiger=0x7f050008;
         public static final int mapView=0x7f050004;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f050002;
-        public static final int value=0x7f050008;
+        public static final int value=0x7f050009;
     }
     public static final class layout {
         public static final int hand_main=0x7f030000;
